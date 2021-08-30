f4931e6b22617e3656aedac2652054e205087c0c
ygkn <2000ygkn0713@gmail.com>
2021-08-30 18:17:20 +0900

    introduce Chakra UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import { NextPage } from "next";
+import Head from "next/head";
 
 const IndexPage: NextPage = () => {
-  return <p>Hello, World!</p>;
+  return (
+    <div>
+      <Head>
+        <title>Caffeine Busters</title>
+      </Head>
+      <p>Hello, World!</p>
+    </div>
+  );
 };
 
 export default IndexPage;
