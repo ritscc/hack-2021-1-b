edc61c33a7fc9aad7bcda7b7ba45fb7aa0c605a7
ygkn <2000ygkn0713@gmail.com>
2021-08-31 08:14:13 +0900

    chore: format index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,22 +8,35 @@ const IndexPage: NextPage = () => {
     <Layout>
       <Head>
         <title>Caffeine Busters</title>
-      </Head> 
-      <Text textAlign="center" style={{fontSize: 30 }}>今日飲めるのは<br/>あと</Text>
-      <Text textAlign="center" style={{fontSize: 50 }}>☕️×n</Text>
+      </Head>
+      <Text textAlign="center" style={{ fontSize: 30 }}>
+        今日飲めるのは
+        <br />
+        あと
+      </Text>
+      <Text textAlign="center" style={{ fontSize: 50 }}>
+        ☕️×n
+      </Text>
       <Flex>
-        <Text flexGrow ={1}>就寝6時間前の<br />19時まで飲めます</Text>
+        <Text flexGrow={1}>
+          就寝6時間前の
+          <br />
+          19時まで飲めます
+        </Text>
         <Box>
           <Button onClick={() => alert("こんにちは")}>就寝時刻の設定</Button>
         </Box>
       </Flex>
-      <VStack align="stretch" gap = "4">
-        <Button size="lg"  onClick={() => alert("こんにちは")}>飲んだ</Button>
-        <Button size="lg"  onClick={() => alert("こんにちは")}>記録を確認</Button>
+      <VStack align="stretch" gap="4">
+        <Button size="lg" onClick={() => alert("こんにちは")}>
+          飲んだ
+        </Button>
+        <Button size="lg" onClick={() => alert("こんにちは")}>
+          記録を確認
+        </Button>
       </VStack>
 
       <Text>カフェインカレンダー</Text>
-
     </Layout>
   );
 };
