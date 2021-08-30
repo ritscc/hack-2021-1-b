e949d5221abfed7d603eb74ad891a0dc0d7ab698
Drunknow <ht.ryusi@gmail.com>
2021-08-31 00:34:45 +0900

    implement index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Text, VStack } from "@chakra-ui/react";
 import { NextPage } from "next";
 import Head from "next/head";
 import { Layout } from "../components/Layout";
@@ -8,11 +8,22 @@ const IndexPage: NextPage = () => {
     <Layout>
       <Head>
         <title>Caffeine Busters</title>
-      </Head>
-      <Text textAlign="center">超カフェインバスターズ</Text>
-      <Box textAlign="center">
-        <Button onClick={() => alert("こんにちは")}>ボタンだよ</Button>
-      </Box>
+      </Head> 
+      <Text textAlign="center" style={{fontSize: 30 }}>今日飲めるのは<br/>あと</Text>
+      <Text textAlign="center" style={{fontSize: 50 }}>☕️×n</Text>
+      <Flex>
+        <Text flexGrow ={1}>就寝6時間前の<br />19時まで飲めます</Text>
+        <Box>
+          <Button onClick={() => alert("こんにちは")}>就寝時刻の設定</Button>
+        </Box>
+      </Flex>
+      <VStack align="stretch" gap = "4">
+        <Button size="lg"  onClick={() => alert("こんにちは")}>飲んだ</Button>
+        <Button size="lg"  onClick={() => alert("こんにちは")}>記録を確認</Button>
+      </VStack>
+
+      <Text>カフェインカレンダー</Text>
+
     </Layout>
   );
 };
