675b3de52e037eb8d15b70af9054817056d02874
ygkn <2000ygkn0713@gmail.com>
2021-08-30 22:31:21 +0900

    add Logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import { Flex, Heading } from "@chakra-ui/react";
 import { VFC } from "react";
+import { Logo } from "./Logo";
 
 export const Header: VFC = () => {
   return (
     <Flex as="header">
-      <Heading>Caffeine Busters</Heading>
+      <Heading color="orange.900" fontSize="3xl">
+        <Logo />
+      </Heading>
     </Flex>
   );
 };
