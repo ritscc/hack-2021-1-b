481a5261ed78d78210c4ad5fe9125204429bcb58
ygkn <2000ygkn0713@gmail.com>
2021-08-31 17:20:38 +0900

    カフェイン一覧画面を修正

--- src/pages/caffeine/index.tsx
+++ src/pages/caffeine/index.tsx
@@ -1,38 +1,61 @@
-import { Flex, Text, VStack,Spacer } from "@chakra-ui/react";
+import { Flex, Text, VStack, Spacer, Box } from "@chakra-ui/react";
 import { NextPage } from "next";
 import Head from "next/head";
+import { useCollection } from "react-firebase-hooks/firestore";
 import { Layout } from "../../components/Layout";
+import firebase from "firebase";
+import { useUser } from "../../lib/user";
+import { Caffeine } from "../../lib/caffeine";
+import dayjs from "dayjs";
+import NextLink from "next/link";
 
 const IndexPage: NextPage = () => {
+  const userState = useUser();
+
+  const [caffeineList, loadingCaffeineList] = useCollection<Caffeine>(
+    userState.user &&
+      firebase.firestore().collection(`/users/${userState.user.id}/caffeine`)
+  );
+
   return (
     <Layout>
       <Head>
         <title>カフェイン記録一覧 | Caffeine Busters</title>
       </Head>
-      <VStack align="stretch" border="solid" borderColor="orange.600">
-        <Flex p="4" borderBottom="solid" borderColor="orange.600">
-            <Text>ドリップコーヒー</Text>
-            <Spacer />
-              <Text>今日</Text>
-        </Flex>
-        <Flex p="4" borderBottom="solid" borderColor="orange.600">
-            <Text>ドリップコーヒー</Text>
-            <Spacer />
-              <Text>今日</Text>
-        </Flex>
-        <Flex p="4" borderBottom="solid" borderColor="orange.600">
-            <Text>ドリップコーヒー</Text>
-            <Spacer />
-              <Text>今日</Text>
-        </Flex>
-        <Flex p="4" borderColor="orange.600">
-            <Text>ドリップコーヒー</Text>
-            <Spacer />
-              <Text>今日</Text>
-        </Flex>
-      </VStack>
+
+      {loadingCaffeineList && <Text align="center">Loading...</Text>}
+      {caffeineList && (
+        <VStack align="stretch">
+          {caffeineList.docs.map((doc, i, self) => {
+            const data = doc.data();
+            const time = dayjs(data.time.toDate());
+            const isLast = i === self.length - 1;
+
+            return (
+              <NextLink href={`/caffeine/${doc.id}`} key={doc.id}>
+                <Flex
+                  as="a"
+                  p="2"
+                  align="center"
+                  borderBottomWidth={!isLast ? "thin" : undefined}
+                >
+                  <Text px="2" fontSize="lg">
+                    {data.beverage}
+                  </Text>
+                  <Spacer />
+                  <Flex flexDir="column" fontSize="sm" textAlign="right">
+                    <Text>{data.amount} mg</Text>
+                    <Text title={time.format("YYYY/MM/DD HH:mm:ss")}>
+                      {time.fromNow()}
+                    </Text>
+                  </Flex>
+                </Flex>
+              </NextLink>
+            );
+          })}
+        </VStack>
+      )}
     </Layout>
-    
   );
 };
 
