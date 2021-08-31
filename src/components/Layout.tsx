d3e5e13927e6e336754918f6b31436bb9f40df2c
ygkn <2000ygkn0713@gmail.com>
2021-09-01 08:56:20 +0900

    fix layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,17 @@
-import { Container, Text, VStack } from "@chakra-ui/react";
+import { Box, Container, Text, VStack } from "@chakra-ui/react";
 import { FC } from "react";
 import { Header } from "./Header";
 
 export const Layout: FC = ({ children }) => (
-  <Container maxW="container.md" py="4">
-    <VStack spacing="4" align="stretch">
+  <Container maxW="container.md">
+    <VStack spacing="4" w="100%" minH="100vh" py="4" alignItems="stretch">
       <Header />
-
-      {children}
+      <VStack flexGrow={1} as="main" alignItems="stretch">
+        {children}
+      </VStack>
+      <Text as="footer" textAlign="center" color="orange.800">
+        © 2021 Caffeine Busters
+      </Text>
     </VStack>
-    <Text as="footer" textAlign="center" color="orange.800">
-      © 2021 Caffeine Busters
-    </Text>
   </Container>
 );
