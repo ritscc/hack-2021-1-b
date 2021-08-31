21d9687a61854205cf88c9ae9dc0ed8c4ecd9f42
ygkn <2000ygkn0713@gmail.com>
2021-09-01 02:25:56 +0900

    show 残機

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,43 @@
 import { Box, Button, Flex, Text, VStack } from "@chakra-ui/react";
 import dayjs from "dayjs";
+import firebase from "firebase";
 import { NextPage } from "next";
 import Head from "next/head";
 import NextLink from "next/link";
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
 import { BedtimeForm } from "../components/BedtimeForm";
 import { Layout } from "../components/Layout";
+import { Caffeine } from "../lib/caffeine";
 import { useUser } from "../lib/user";
 
 const IndexPage: NextPage = () => {
   const userState = useUser();
   const [showBedtimeForm, setShowBedtimeForm] = useState<boolean>(false);
+
+  const query = useMemo(
+    () =>
+      userState.user &&
+      firebase
+        .firestore()
+        .collection(`/users/${userState.user.id}/caffeine`)
+        .where(
+          "time",
+          ">",
+          firebase.firestore.Timestamp.fromDate(
+            dayjs().subtract(1, "day").toDate()
+          )
+        )
+        .orderBy("time"),
+    [userState.user]
+  );
+
+  const [todayCaffeineList] = useCollection<Caffeine>(query);
+
+  const todayCaffeineTotal = todayCaffeineList?.docs.reduce(
+    (previous, current) => previous + current.data().amount,
+    0
+  );
 
   return (
     <Layout>
@@ -24,7 +51,7 @@ const IndexPage: NextPage = () => {
         </NextLink>
       )}
 
-      {userState.state === "LOADED" && (
+      {userState.state === "LOADED" && todayCaffeineTotal !== undefined && (
         <VStack spacing="4" align="stretch">
           <Text textAlign="center" style={{ fontSize: 30 }}>
             今日飲めるのは
@@ -32,7 +59,7 @@ const IndexPage: NextPage = () => {
             あと
           </Text>
           <Text textAlign="center" style={{ fontSize: 50 }}>
-            ☕️×n
+            ☕️× {Math.floor((400 - todayCaffeineTotal) / 84)} 杯
           </Text>
           <Flex>
             {userState.user.bedtime !== null && (
