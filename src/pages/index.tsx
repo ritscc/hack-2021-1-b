3c58ace832093bc4416ee0f87a795758b93f86a6
河合賢伸 <k-kawai@entaworks.co.jp>
2021-09-01 01:31:02 +0900

    add BedtimeForm to indexPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import { Box, Button, Flex, Text, VStack } from "@chakra-ui/react";
+import dayjs from "dayjs";
 import { NextPage } from "next";
 import Head from "next/head";
+import NextLink from "next/link";
+import { useState } from "react";
+import { BedtimeForm } from "../components/BedtimeForm";
 import { Layout } from "../components/Layout";
 import { useUser } from "../lib/user";
-import NextLink from "next/link";
 
 const IndexPage: NextPage = () => {
   const userState = useUser();
+  const [showBedtimeForm, setShowBedtimeForm] = useState<boolean>(false);
 
   return (
     <Layout>
@@ -31,17 +35,33 @@ const IndexPage: NextPage = () => {
             ☕️×n
           </Text>
           <Flex>
-            <Text flexGrow={1}>
-              就寝6時間前の
-              <br />
-              19時まで飲めます
-            </Text>
+            {userState.user.bedtime !== null && (
+              <Text flexGrow={1}>
+                就寝6時間前の
+                <br />
+                {dayjs(userState.user.bedtime, "HH:mm")
+                  .subtract(6, "hour")
+                  .format("HH:mm")}
+                まで飲めます
+              </Text>
+            )}
+            {userState.user.bedtime === null && (
+              <Text flexGrow={1}>就寝時刻が未設定です</Text>
+            )}
+
             <Box>
-              <Button onClick={() => alert("こんにちは")}>
+              <Button onClick={() => setShowBedtimeForm(!showBedtimeForm)}>
                 就寝時刻の設定
               </Button>
             </Box>
           </Flex>
+          {showBedtimeForm && (
+            <BedtimeForm
+              onClose={() => {
+                setShowBedtimeForm(false);
+              }}
+            />
+          )}
           <VStack align="stretch" gap="4">
             <NextLink href="/caffeine/new">
               <Button size="lg" as="a">
