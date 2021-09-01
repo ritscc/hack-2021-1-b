0a366bd287061bca44063b820b3791aabfaca2e9
ygkn <2000ygkn0713@gmail.com>
2021-09-01 09:03:13 +0900

    add PrivatePage component

---
+++ src/components/PrivatePage.tsx
@@ -0,0 +1,38 @@
+import { Center, Spinner } from "@chakra-ui/react";
+import { VFC } from "react";
+import { User, useUser } from "../lib/user";
+
+type Props = {
+  renderOnSignedIn: (user: User) => JSX.Element;
+  renderOnUnauthorized?: () => JSX.Element;
+};
+
+export const PrivatePage: VFC<Props> = ({
+  renderOnSignedIn,
+  renderOnUnauthorized,
+}) => {
+  const userState = useUser();
+
+  console.log(userState);
+
+  if (userState.state === "UNAUTHORIZED") {
+    if (renderOnUnauthorized !== undefined) {
+      return renderOnUnauthorized();
+    }
+    return (
+      <Center h="100%" w="100%" m="auto">
+        ログインしてね！
+      </Center>
+    );
+  }
+
+  if (userState.state === "LOADED") {
+    return renderOnSignedIn(userState.user);
+  }
+
+  return (
+    <Center h="100%" w="100%" m="auto">
+      <Spinner size="xl" />
+    </Center>
+  );
+};
