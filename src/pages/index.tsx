d6efa503073aeb1be0cc40d33fb570e82217d96f
ygkn <2000ygkn0713@gmail.com>
2021-09-01 10:19:55 +0900

    就寝6時間前を過ぎていた場合警告

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,53 +20,67 @@ const IndexPage: NextPage = () => {
       </Head>
 
       <PrivatePage
-        renderOnSignedIn={(user) => (
-          <VStack spacing="4" align="stretch">
-            <RemainingCoffees />
-            <Flex align="center">
-              {user.bedtime !== null && (
-                <Text flexGrow={1}>
-                  就寝6時間前の
-                  <br />
-                  {dayjs(user.bedtime, "HH:mm")
-                    .subtract(6, "hour")
-                    .format("HH:mm")}
-                  まで飲めます
-                </Text>
-              )}
-              {user.bedtime === null && (
-                <Text flexGrow={1}>就寝時刻が未設定です</Text>
-              )}
+        renderOnSignedIn={(user) => {
+          const caffeineTimeLimit = dayjs(user.bedtime, "HH:mm") //
+            .subtract(6, "hour");
 
-              <Box>
-                <Button onClick={() => setShowBedtimeForm(!showBedtimeForm)}>
-                  就寝時刻の設定
-                </Button>
-              </Box>
-            </Flex>
-            {showBedtimeForm && (
-              <BedtimeForm
-                onClose={() => {
-                  setShowBedtimeForm(false);
-                }}
-              />
-            )}
-            <VStack align="stretch" gap="4">
-              <NextLink href="/caffeine/new">
-                <Button size="lg" as="a">
-                  飲んだ
-                </Button>
-              </NextLink>
-              <NextLink href="/caffeine/">
-                <Button size="lg" as="a">
-                  記録を確認
-                </Button>
-              </NextLink>
+          const caffeineTimeLimitText = caffeineTimeLimit.format("HH:mm");
+
+          const isAfterCaffeineTimeLimitText =
+            dayjs().isAfter(caffeineTimeLimit);
+
+          return (
+            <VStack spacing="4" align="stretch">
+              <RemainingCoffees />
+              <Flex align="center">
+                {user.bedtime !== null && !isAfterCaffeineTimeLimitText && (
+                  <Text flexGrow={1}>
+                    就寝6時間前の
+                    {caffeineTimeLimitText}
+                    までは飲めます
+                  </Text>
+                )}
+                {user.bedtime !== null && isAfterCaffeineTimeLimitText && (
+                  <Text flexGrow={1} color="red.500">
+                    就寝6時間前の
+                    {caffeineTimeLimitText}
+                    を過ぎています。 もう飲まないようにしましょう。
+                  </Text>
+                )}
+                {user.bedtime === null && (
+                  <Text flexGrow={1}>就寝時刻が未設定です</Text>
+                )}
+
+                <Box>
+                  <Button onClick={() => setShowBedtimeForm(!showBedtimeForm)}>
+                    就寝時刻の設定
+                  </Button>
+                </Box>
+              </Flex>
+              {showBedtimeForm && (
+                <BedtimeForm
+                  onClose={() => {
+                    setShowBedtimeForm(false);
+                  }}
+                />
+              )}
+              <VStack align="stretch" gap="4">
+                <NextLink href="/caffeine/new">
+                  <Button size="lg" as="a">
+                    飲んだ
+                  </Button>
+                </NextLink>
+                <NextLink href="/caffeine/">
+                  <Button size="lg" as="a">
+                    記録を確認
+                  </Button>
+                </NextLink>
+              </VStack>
+
+              <CaffeineCalender />
             </VStack>
-
-            <CaffeineCalender />
-          </VStack>
-        )}
+          );
+        }}
       />
     </Layout>
   );
